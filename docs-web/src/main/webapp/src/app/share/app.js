f2bf9bcab81ffdaea74d21bb64bac6783e63ed8f
Prakash <ngprakasam@gmail.com>
2019-11-30 08:48:21 +0530

    Update app.js
    name changed

--- docs-web/src/main/webapp/src/app/share/app.js
+++ docs-web/src/main/webapp/src/app/share/app.js
@@ -132,6 +132,6 @@ angular.module('share',
   // Fetch the current theme configuration
   $rootScope.appName = '';
   Restangular.one('theme').get().then(function(data) {
-    $rootScope.appName = data.name;
+    $rootScope.appName = 'dAppBox';
   });
 });
